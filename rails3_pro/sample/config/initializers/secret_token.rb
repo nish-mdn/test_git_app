577cca34be59e689232f8d901e084cc67944f6fc
Nisha <madhantry@gmail.com>
2012-06-23 12:48:01 +0530

    added sample rails 3 pro via wifi network

---
+++ rails3_pro/sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '457c5a950d8efe9e8b02e236dcd70974125b8e26b40f6018abc4002d07d38d41de2b16c75d0e286b91cb687044770d51a74bcece77eab7c4b8c0be376edeb702'
