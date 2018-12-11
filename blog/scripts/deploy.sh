a15510c4828e3406141d6ab1f2de176080cb4799
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-11 16:05:18 +0530

    rails server start comment added with -d

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.207.183.128:~/.
-sudo ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.207.183.128
-sudo cd blog && rails s -p3000 -d
+sudo ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.207.183.128 cd blog | rails s -p3000 -d
