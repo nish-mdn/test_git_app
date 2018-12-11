d3efc15adbe4c80381550c7be398c599fd9b8756
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-11 13:07:10 +0530

    directory listing

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "going to list file"
-comd="ls -la ~/"
+comd="ls -la $TRAVIS_BUILD_DIR"
 eval "$comd"
 sudo scp -i ../mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ~/blog/* ubuntu@18.207.183.128:~/.
