70e204541040cd06661b26fa4d107fe223de0bab
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-11 15:13:56 +0530

    scp recurse option added

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo scp -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.207.183.128:~/.
+sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.207.183.128:~/.
