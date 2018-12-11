5620a41e4fb26a21fa1087c74733c553fb5facf0
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-11 12:58:25 +0530

    directory listing

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "going to list file"
-eval(ls -la ~/)
+comd="ls -la ~/"
+eval "$comd"
 sudo scp -i ../mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ~/blog/* ubuntu@18.207.183.128:~/.
