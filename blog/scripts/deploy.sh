079e8d9d7354f72bae8a7a6b32861c9dd9dcae2d
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-11 14:43:00 +0530

    directory listing-2

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo "going to list file"
-comd="locate mdn-mysql-own.pem"
-eval "$comd"
-sudo scp -i mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/* ubuntu@18.207.183.128:~/.
+sudo scp -i mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog ubuntu@18.207.183.128:~/.
