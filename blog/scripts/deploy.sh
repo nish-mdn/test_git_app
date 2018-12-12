0a890fb25570dc4e079dcf5ffd56597ad95537b3
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-12 18:02:37 +0530

    kill added back

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.204.243.94:~/.
-ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.204.243.94 "source ~/.bash_profile; cd blog && rvm use ruby-2.3.1 && bundle install && rails s -b 0.0.0.0 -d"
+ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.204.243.94 "source ~/.bash_profile; cd blog && kill -9 $(cat blog/tmp/pids/server.pid) && rvm use ruby-2.3.1 && bundle install && rails s -b 0.0.0.0 -d"
