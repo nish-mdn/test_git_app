4ae40e643949f38158d6b5f17901a562938509a0
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-12 15:04:59 +0530

    new public ip changes

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.207.183.128:~/.
-ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.214.36.199 "source ~/.bash_profile; cd blog && rvm use ruby-2.3.1 && bundle install && rails s -p3000 -d"
+sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.204.243.94:~/.
+ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.204.243.94 "source ~/.bash_profile; cd blog && rvm use ruby-2.3.1 && bundle install && rails s -p3000 -d"
