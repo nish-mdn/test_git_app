fff8fafe9cb968aecc72473aa7135eb85e54849e
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-13 20:08:37 +0530

    path change done

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.232.146.23:~/.
-ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.232.146.23 /bin/bash < rails_server_startup.sh
+ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.232.146.23 /bin/bash < /home/travis/build/nish-mdn/test_git_app/blog/scripts/rails_server_startup.sh
