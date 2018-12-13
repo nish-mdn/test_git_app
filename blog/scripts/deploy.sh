8d13112a537a581b1cefa83d038915c604faf4b7
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-13 15:02:35 +0530

    kill command reoved

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@18.232.146.23:~/.
-ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.232.146.23 "source ~/.bash_profile; cd blog && kill -9 $(cat tmp/pids/server.pid) && rvm use ruby-2.3.1 && bundle install && rails s -b 0.0.0.0 -d"
+ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@18.232.146.23 "source ~/.bash_profile; cd blog && rvm use ruby-2.3.1 && bundle install && rails s -b 0.0.0.0 -d"
