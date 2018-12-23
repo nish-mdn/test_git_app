53a52eae76d798439adb18f5a427af636b971c8b
Madhan Ayyasamy <madhan.ayyasamy@idexcel.com>
2018-12-23 20:59:15 +0530

    deploy trigger

--- blog/scripts/aws_instance_creation.rb
+++ blog/scripts/aws_instance_creation.rb
@@ -79,4 +79,4 @@ else
 end
 ip_address_for_target_machine = infra.get_instance_ip
 puts "infra ip #{ip_address_for_target_machine}"
-system("./deploy.sh #{ip_address_for_target_machine}")
+system("./deploy.sh '#{ip_address_for_target_machine}' ")
