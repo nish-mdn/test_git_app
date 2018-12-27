85a80cec08630409c387dba5af6e9b122f1285c1
Madhan Ayyasamy <madhantry@gmail.com>
2018-12-27 20:08:22 +0530

    ip changed

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@100.27.38.68:~/.
-ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@100.27.38.68 /bin/bash < /home/travis/build/nish-mdn/test_git_app/blog/scripts/rails_server_startup.sh
+ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@34.236.243.77 /bin/bash < /home/travis/build/nish-mdn/test_git_app/blog/scripts/rails_server_startup.sh
 
