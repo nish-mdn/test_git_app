49021d5e8888a2a3363865243cd0b6cbc259299a
Madhan Ayyasamy <madhantry@gmail.com>
2018-12-28 11:00:03 +0530

    public ip mentioned in master branch

--- blog/scripts/deploy.sh
+++ blog/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@34.236.243.77:~/.
-ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@34.236.243.77 /bin/bash < /home/travis/build/nish-mdn/test_git_app/blog/scripts/rails_server_startup.sh
+sudo scp -r -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" /home/travis/build/nish-mdn/test_git_app/blog/ ubuntu@34.200.233.82:~/.
+ssh -i /home/travis/build/nish-mdn/test_git_app/blog/mdn-mysql-own.pem -o "StrictHostKeyChecking=no" ubuntu@34.200.233.82 /bin/bash < /home/travis/build/nish-mdn/test_git_app/blog/scripts/rails_server_startup.sh
 
