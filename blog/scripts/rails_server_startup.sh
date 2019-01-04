a5103d91772f01f423b5dcea34c71ac7e9dff898
Madhan Ayyasamy <madhantry@gmail.com>
2019-01-04 19:41:01 +0530

    few more changes

--- blog/scripts/rails_server_startup.sh
+++ blog/scripts/rails_server_startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source ~/.bash_profile;
 kill -9 $(lsof -i tcp:3000 -t)
-rvm use ruby-2.3.1 && bundle install && rails s -b 0.0.0.0 -d
+echo 'Benchmarking $(pwd)...'
+cd /home/ubuntu/blog/ && rvm use ruby-2.3.1 && bundle install && rails s -b 0.0.0.0 -d
